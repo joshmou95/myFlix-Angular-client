c3380f86853c3b800a633eabec1023a940572f97
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-10-04 11:12:22 -0600

    Inject director data

--- src/app/director-dialog/director-dialog.component.ts
+++ src/app/director-dialog/director-dialog.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-dialog',
   templateUrl: './director-dialog.component.html',
   styleUrls: ['./director-dialog.component.scss']
 })
+
 export class DirectorDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: any) { }
 
   ngOnInit(): void {
   }
