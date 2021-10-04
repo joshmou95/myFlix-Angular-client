bd0c760cfebea1e001107c5a475e6778b5435e2a
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-03 18:21:10 -0600

    Create welcome-page.component.ts

---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+// import app components
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss']
+})
+
+export class WelcomePageComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  // This will open the registration dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+    // Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+  // This will open the registration dialog when the Login button is clicked  
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+    // Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+  // Opens the movie dialog
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
+
+
+
+}
