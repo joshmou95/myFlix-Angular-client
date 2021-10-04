78bfde1a62041c57b5232e7a2ff7edb0e50867f2
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-10-04 13:00:26 -0600

    inject data

--- src/app/details-dialog/details-dialog.component.ts
+++ src/app/details-dialog/details-dialog.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-details-dialog',
   templateUrl: './details-dialog.component.html',
   styleUrls: ['./details-dialog.component.scss']
 })
+
 export class DetailsDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: any) { }
 
   ngOnInit(): void {
   }
