7ddf1260c2d72f9fc5e7de8538ae88cb6ae6f9bb
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-05 22:25:25 -0600

    update snack message

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -34,8 +34,8 @@ export class UserRegistrationFormComponent implements OnInit {
     // Logic for a successful user registration goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
       console.log(response);
-      this.snackBar.open(response, 'OK', {
-          duration: 2000
+      this.snackBar.open(`You are now registered and can log in!`, 'OK', {
+          duration: 3000
       });
       }, (response) => {
         console.log(response);
