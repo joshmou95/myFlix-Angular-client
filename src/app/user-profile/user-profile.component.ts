5244d774c1d29ed885a3e91c9d6680a591dd5eab
Joshua Mouton <80426764+joshmou95@users.noreply.github.com>
2021-10-08 17:46:54 -0600

    Add delete user, get favs

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,11 +10,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserProfileComponent implements OnInit {
   user: any = {};
+  favorites: any[] = [];
   username = localStorage.getItem('user');
 
 
   constructor(
     public fetchApiData: FetchApiDataService,
+    public router: Router,
     public snackBar: MatSnackBar
   ) { }
 
@@ -28,5 +30,36 @@ export class UserProfileComponent implements OnInit {
       console.log('getUserData reached', resp)
     });
   }
+
+
+  // get FavoriteMovies array from user object
+  getUserFavs(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) =>{
+      this.favorites = resp.FavoriteMovies;
+      return this.favorites
+    });
+  }
+
+  deleteUserData(): void {
+    let check = confirm(
+      'This will delete your profile! Are you sure you want to continue?'
+    );
+    if (check) {
+      this.fetchApiData.deleteUser().subscribe(() => {
+        localStorage.clear();
+        this.router.navigate(['welcome']);
+        this.snackBar.open('Profile deleted', 'OK', {
+          duration: 2000,
+        });
+      });
+    } else {
+      window.location.reload();
+    }
+  }
+  // profileUpdateDialog(): void {
+  //   this.dialog.open(UserProfileUpdateComponent, {
+  //     panelClass: 'update-dialog',
+  //   });
+  // }
 
 }
