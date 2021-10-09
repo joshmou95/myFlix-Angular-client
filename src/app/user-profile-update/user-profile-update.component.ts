7e8f0f96a0aa13b0bd3df51a4872b2a4d7ccff4f
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-08 23:33:35 -0600

    Add updateUser function

--- src/app/user-profile-update/user-profile-update.component.ts
+++ src/app/user-profile-update/user-profile-update.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-profile-update',
   templateUrl: './user-profile-update.component.html',
   styleUrls: ['./user-profile-update.component.scss']
 })
+
 export class UserProfileUpdateComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserProfileUpdateComponent>,
+    public snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  // This is the function responsible for sending the form inputs to the backend
+  updateUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((response) => {
+
+  // Logic for a successful user registration goes here! (To be implemented)
+    this.dialogRef.close(); 
+    // This will close the modal on success!
+    localStorage.setItem('username', response.Username)
+    console.log(response);
+    this.snackBar.open(`Update Successful!`, 'OK', {
+        duration: 3000
+    });
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
