60d02e625665ea9beaf1542e9fd7538566684888
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-09 16:26:50 -0600

    filter favorites array

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-update.component';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -13,7 +14,8 @@ import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-
 
 export class UserProfileComponent implements OnInit {
   user: any = {};
-  favorites: any[] = [];
+  favorites: any = [];
+  movies: any[] = [];
   username = localStorage.getItem('user');
 
 
@@ -24,37 +26,56 @@ export class UserProfileComponent implements OnInit {
     public dialog: MatDialog,
   ) { }
 
+  // gets user data upon page load
   ngOnInit(): void {
     this.getUserData();
   }
 
+  // get user object with username, email, birthday, and Favorite movies triggers getMovies()
   getUserData(): void {
     this.fetchApiData.getUser().subscribe((resp: any) => {
       this.user = resp;
-      console.log('getUserData reached', resp)
+      this.getMovies();
     });
   }
 
-  openProfileUpdateDialog(): void {
+  // gets Movies data and triggers filterFavorites
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      this.filterFavorites();
+    });
+  }
+
+  // filter Favorite movies from movies data returns user favorites to array
+  filterFavorites(): void {
+    this.movies.forEach((movie: any) => {
+      if (this.user.FavoriteMovies.includes(movie._id)) {
+        this.favorites.push(movie);
+      }
+    });
+    return this.favorites;
+  }
+    
+  // with "Update Profile" button is pressed, it passes data to UserProfileUpdateComponent for update
+  openProfileUpdateDialog(username: string, email: string, birthday: string): void {
+    // pass data to user profile update component
     this.dialog.open(UserProfileUpdateComponent, {
+      data: {
+        username: username,
+        email: email,
+        birthday: birthday
+      },
       width: '250px'
     });
   }
 
-
-  // get FavoriteMovies array from user object
-  getUserFavs(): void {
-    this.fetchApiData.getUser().subscribe((resp: any) =>{
-      this.favorites = resp.FavoriteMovies;
-      return this.favorites
-    });
-  }
-
+  // when "Delete Account" button is pressed it deletes the user from the API after confirmation
   deleteUserData(): void {
     let check = confirm(
       'This will delete your profile! Are you sure you want to continue?'
     );
-    if (check) {
+    if (check == true) {
       this.fetchApiData.deleteUser().subscribe(() => {
         localStorage.clear();
         this.router.navigate(['welcome']);
@@ -66,10 +87,4 @@ export class UserProfileComponent implements OnInit {
       this.router.navigate(['users']);
     }
   }
-  // profileUpdateDialog(): void {
-  //   this.dialog.open(UserProfileUpdateComponent, {
-  //     panelClass: 'update-dialog',
-  //   });
-  // }
-
 }
