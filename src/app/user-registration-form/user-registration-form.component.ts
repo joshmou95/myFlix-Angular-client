797bbb0b9ac447097ce0cf3122ae0d2e1cc20dfb
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-09 16:27:08 -0600

    add loading state and comments

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -6,6 +6,7 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+
 // component can be used in the html file as <app-user-registration-form>
 @Component({
   selector: 'app-user-registration-form',
@@ -13,8 +14,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-registration-form.component.scss']
 })
 
-
 export class UserRegistrationFormComponent implements OnInit {
+  isLoading = false;
 
   // defines the components input, userData object is passed to the API call in the registerUser function
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
@@ -27,17 +28,20 @@ export class UserRegistrationFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // This is the function responsible for sending the form inputs to the backend
+  // Send the form inputs to register user at the API
   registerUser(): void {
+      this.isLoading = true;
       this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-
-    // Logic for a successful user registration goes here! (To be implemented)
-      this.dialogRef.close(); // This will close the modal on success!
-      console.log(response);
+      this.isLoading = false;
+      // This will close the modal on success!
+      this.dialogRef.close(); 
+      console.log('registerUser successful');
+      // message to user when registration is a success
       this.snackBar.open(`You are now registered and can log in!`, 'OK', {
           duration: 3000
       });
       }, (response) => {
+        this.isLoading = false;
         console.log(response);
         this.snackBar.open(response, 'OK', {
           duration: 2000
