31223614ba0998db54411a98c0310feb3250d90f
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-10 17:31:19 -0600

    Add comments

--- src/app/genre-dialog/genre-dialog.component.ts
+++ src/app/genre-dialog/genre-dialog.component.ts
@@ -7,10 +7,15 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./genre-dialog.component.scss']
 })
 
+/**
+ * This class displays moive genre name and description
+ */
 export class GenreDialogComponent implements OnInit {
 
   constructor(
-    // retrieve data from movie-card openGenreDialog()
+    /**
+     * retrieve data from movie-card openGenreDialog()
+     */
     @Inject(MAT_DIALOG_DATA)
     public data: any) { }
 
